0ef9f6e0c808889f2f73373f686309905a6abb4e
Patrick-WatsonJr <71508662+Patrick-WatsonJr@users.noreply.github.com>
2020-10-24 09:58:36 -0400

    Add files via upload

---
+++ email.py
@@ -0,0 +1,28 @@
+"""
+
+This is a script that prompts the user to enter email addresses which adds them to
+a list and prints the list.
+
+"""
+
+
+#email address
+
+addresses = []
+
+#asks user if they have an email address to enter, user must input yes or no
+more = input("Do you have an email address to enter (y/n)? ")
+
+#asks users to for email address to enter
+while more == "y":
+    email = input("Enter the address: ")
+    addresses.append(email)
+#Asks user to input another address
+    more = input("Do you have another address(y/n)? ")
+    while more != "y":
+        if more == "n":
+            break
+        else:
+            more = input("Please enter a y or n: ")
+#prints email addresses    
+print(addresses)
